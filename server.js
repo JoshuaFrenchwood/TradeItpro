dad83c85f6d30461b7dfffce9b426a708dfb6c67
Joshua Frenchwood <joshua.frenchwood@gmail.com>
2019-08-28 22:00:34 -0500

    Commit

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ const port=process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-
-
 //Conect to MongodB through mongoose
 
 connectDB();
@@ -42,7 +40,7 @@ app.use('/api/stockprice',stockprice);
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-  
+
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
